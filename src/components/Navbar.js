381ccff8fd9143639e11d0a7caba79f6168ea158
S8leeJ <jenna.snow.lee@gmail.com>
2025-06-26 16:30:44 -0500

    navbarFix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav className="px-6 py-5 flex justify-between items-center text-2xl">
-      <h1 className="text-2xl font-bold text-yellow-100">Jenna's Portfolio</h1>
-      <div className="space-x-11">
+      <h1 className="text-1xl font-bold text-yellow-100">Jenna's Portfolio</h1>
+      <div className="space-x-5">
         <Link to="/" className="text-white-400 hover:text-purple-400">Home</Link>
         <Link to="/about" className="text-white-400 hover:text-purple-400">About</Link>
         <Link to="/projects" className="text-white-400 hover:text-purple-400">Projects</Link>
